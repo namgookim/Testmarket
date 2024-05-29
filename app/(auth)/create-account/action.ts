c9e472a4974186c2f443069e1721f549d89a60fa
Kim-NamGoo <namgookim2@gmail.com>
2024-05-30 01:05:23 +0900

    ~10.0
    10강까지의 진도

---
+++ app/(auth)/create-account/action.ts
@@ -0,0 +1,67 @@
+"use server";
+
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import { PASSWORD_REGEX, PASSWORD_REGEX_ERROR, PASSWORD_MIN_LENGTH } from "@/app/lib/constants";
+import db from "@/app/lib/db";
+import { redirect } from "next/navigation";
+import getSession from "@/app/lib/session";
+
+const checkUsername = (username: string) => !username.includes("potato");
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string!",
+        required_error: "Where is my username???",
+      })
+      .toLowerCase()
+      .trim()
+      .transform((username) => `🔥 ${username} 🔥`),
+    email: z.string().email().toLowerCase(),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    //.regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same!",
+    path: ["confirm_password"],
+  });
+export async function createAccount(prevState: any, formData: FormData) {
+  const data = {
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+    confirm_password: formData.get("confirm_password"),
+  };
+
+  const result = await formSchema.spa(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const session = await getSession();
+    session.id = user.id;
+    await session.save();
+    redirect("/profile");
+  }
+  // redirect "/home"
+}
